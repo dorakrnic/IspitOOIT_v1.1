58900e52fe1bbbe37a12b4270ecc74e3580a8dae
Vukmanovic <Administrator@NAS.itc03.edu>
2015-11-27 14:25:26 +0100

    Vezbe 7 24.11.2015. (dopuna)

---
+++ OOIT IIM 2/src/geometrija/Crtez.java
@@ -0,0 +1,49 @@
+package geometrija;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Crtez extends JPanel{
+
+	public static void main(String[] args) {
+		JFrame prozor = new JFrame();
+		prozor.setSize(800, 600);
+		prozor.setVisible(true);
+		prozor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		prozor.setTitle("Vidi prozor!!!");
+		Crtez c = new Crtez();
+		prozor.getContentPane().add(c);
+		c.setBackground(Color.black);
+			
+	}
+	
+	public void paint (Graphics g){
+		g.drawString("Neki text na poziciji 150 150", 150, 150);
+		Tacka t1 = new Tacka(50, 50);
+		t1.crtajSe(g);
+		Tacka t2 = new Tacka(75, 75);
+		
+		
+		t2.crtajSe(g);
+		t2.pomeriNa(10, 10);
+		t2.crtajSe(g);
+		repaint();
+		Linija l1= new Linija(new Tacka(300, 300), new Tacka(350, 350));
+		l1.crtajSe(g);
+		Pravougaonik p1 = new Pravougaonik(new Tacka(500, 500), 100, 50);
+		p1.crtajSe(g);
+		Kvadrat k1 = new Kvadrat(new Tacka(250, 100), 100);
+		k1.crtajSe(g);
+		Krug kr1 = new Krug(new Tacka(400, 100), 50 );
+		kr1.crtajSe(g);
+		kr1.getCentar().crtajSe(g);
+		//p1.dijagonala().crtajSe(g);
+		k1.dijagonalaKvadrata().crtajSe(g);
+		p1.centarPravougaonika().crtajSe(g);
+		p1.getGoreLevo().crtajSe(g);
+	}
+
+}
